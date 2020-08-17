919f53ca4895a07a8b3e8466fe90e6ad34c8dc28
Jisun <54903487+jisunnchoi@users.noreply.github.com>
2020-08-17 22:50:37 +0900

    python_practice14

---
+++ ternary expression.py
@@ -0,0 +1,17 @@
+#basic 
+condition = True
+    
+if condition:
+    condition_string = "nice"
+else:
+    condition_string = "not nice"
+    
+print(condition_string)      # => nice
+
+#ternary expression
+condition = True
+    
+condition_string = "nice" if condition else "not nice"
+    
+print(condition_string)      # => nice
+
